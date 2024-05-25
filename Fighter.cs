e8e331f58d67bdf6776e99b4b6a15ee2c0fc8d84
mgdlnivnv <stu2301561050@uni-plovdiv.bg>
2024-05-25 23:15:15 +0300

    Add files via upload
    New Class Heroes

---
+++ Fighter.cs
@@ -0,0 +1,64 @@
+﻿using ArenaGameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    internal class Fighter
+    {
+    }
+}
+// - Has a 25% chance to take only half damage.
+// - Has a 30% chance to deal extra damage equal to his Strength with each attack.
+// - Each successful attack temporarily reduces incoming damage by 10% for the next turn.
+public class Fighter : Hero
+{
+    private static readonly Random random = new Random();
+    private bool damageReductionActive;
+
+    public Fighter() : this("Dobby") // By default, all fighters are named 'Dobby'
+    {
+
+    }
+    public Fighter(string name) : base(name) 
+    {
+        damageReductionActive = false;
+    }
+
+    public override int Attack()
+    {
+        int baseAttack = base.Attack();
+        int chance = random.Next(100);
+
+        if (chance < 30) // 30% chance to deal extra damage equal to his Strength
+        {
+            baseAttack += this.Strength;
+        }
+
+        // Activate damage reduction for the next turn
+        damageReductionActive = true;
+
+        return baseAttack;
+    }
+
+    public override void TakeDamage(int incomingDamage)
+    {
+        int chance = random.Next(100);
+
+        if (chance < 25) // 25% chance to take only half damage
+        {
+            incomingDamage /= 2;
+        }
+
+        if (damageReductionActive)
+        {
+            incomingDamage -= (incomingDamage * 10) / 100; // Reduce damage by 10%
+            damageReductionActive = false; // Reset damage reduction
+        }
+
+        base.TakeDamage(incomingDamage);
+    }
+}
---
+++ Villain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGameEngine
+{
+    // - Has a 20% chance to avoid taking damage completely.
+    // - Has a 40% chance to deal damage equal to 2 times his Strength.
+    // - Gains 10% of the dealt damage as health recovery with each attack.
+    internal class Villain
+    {
+        private static readonly Random random = new Random();
+
+        public Villain(string name) : base(name)
+        {
+
+        }
+                   public override int Attack()
+        {
+            int baseAttack = base.Attack();
+            int chance = random.Next(100);
+
+            if (chance < 40) // 40% chance to deal damage equal to 2 times his Strength
+            {
+                baseAttack = baseAttack * 2;
+            }
+
+            // Heal 10% of the attack damage
+            this.Heal(baseAttack / 10);
+
+            return baseAttack;
+        }
+        public override void TakeDamage(int incomingDamage)
+        {
+            int chance = random.Next(100);
+
+            if (chance < 20) // 20% chance to avoid damage completely
+            {
+                incomingDamage = 0;
+            }
+
+            base.TakeDamage(incomingDamage);
+        }
+
+        private void Heal(int amount)
+        {
+            Health += amount;
+            if (Health > 500) // Assuming 500 is the max health
+            {
+                Health = 500;
+            }
+        }
+    }
+}
